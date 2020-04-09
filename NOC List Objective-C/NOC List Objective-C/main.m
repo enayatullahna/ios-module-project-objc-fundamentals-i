b95a5c275e12676d304d62c51b9633ccba6dbe5d
Enayatullah Naseri <enayatullahna@gmail.com>
2020-04-08 18:10:56 -0700

    Created Project

---
+++ NOC List Objective-C/NOC List Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOC List Objective-C
+//
+//  Created by Enayatullah Naseri on 4/8/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
